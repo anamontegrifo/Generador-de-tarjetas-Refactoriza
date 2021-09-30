79f05196e9b283f7585c588ea6a472484f34f04d
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-30 17:09:06 +0200

    api.js created and handled

--- src/components/App.js
+++ src/components/App.js
@@ -1,120 +1,126 @@
 import '../styles/App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './Header';
 import Preview from './Preview';
 import Form from './Form';
 import Footer from './Footer';
-import ImageReader from './ImageReader';
-import picRandom from "../images/picrandom.jpg";
+import picRandom from '../images/picrandom.jpg';
+import Api from '../service/Api';
 
 function App() {
-	const [mode1, setMode1] = useState('');
-	const [mode2, setMode2] = useState('hidden');
-	const [mode3, setMode3] = useState('hidden');
-	const [rotate1, setRotate1] = useState('down');
-	const [rotate2, setRotate2] = useState('up');
-	const [rotate3, setRotate3] = useState('up');
-	const [palette, setPalette] = useState('palette1');
-	const [image, setImage] = useState(picRandom);
+  const [mode1, setMode1] = useState('');
+  const [mode2, setMode2] = useState('hidden');
+  const [mode3, setMode3] = useState('hidden');
+  const [rotate1, setRotate1] = useState('down');
+  const [rotate2, setRotate2] = useState('up');
+  const [rotate3, setRotate3] = useState('up');
+  const [palette, setPalette] = useState('palette1');
+  const [image, setImage] = useState(picRandom);
+  const [twitter, setTwitter] = useState('');
+  const [data, setData] = useState({
+    palette: '',
+    name: '',
+    job: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: '',
+  });
 
-	const [data, setData] = useState({
-		palette: '',
-		name: '',
-		job: '',
-		phone: '',
-		email: '',
-		linkedin: '',
-		github: '',
-	});
+  useEffect(() => {
+    Api().then((response) => {
+      setTwitter(response);
+    });
+  }, []);
 
-	const handleImage = (imageData) => {
-		setImage(imageData);
-	};
+  const handleImage = (imageData) => {
+    setImage(imageData);
+  };
 
-	const handlePalette = (event) => {
-		setPalette(event.target.id);
-	};
+  const handlePalette = (event) => {
+    setPalette(event.target.id);
+  };
 
-	const handleInput = (event) => {
-		const wichInput = event.currentTarget.name;
+  const handleInput = (event) => {
+    const wichInput = event.currentTarget.name;
 
-		if (wichInput === 'name') {
-			setData({
-				...data,
-				name: event.currentTarget.value,
-			});
-			data.name = event.currentTarget.value;
-		} else if (wichInput === 'job') {
-			setData({
-				...data,
-				job: event.currentTarget.value,
-			});
-			data.job = event.currentTarget.value;
-		} else if (wichInput === 'phone') {
-			setData({
-				...data,
-				phone: event.currentTarget.value,
-			});
-			data.phone = event.currentTarget.value;
-		} else if (wichInput === 'email') {
-			setData({
-				...data,
-				email: event.currentTarget.value,
-			});
-			data.email = event.currentTarget.value;
-		} else if (wichInput === 'linkedin') {
-			setData({
-				...data,
-				linkedin: event.currentTarget.value,
-			});
-			data.linkedin = event.currentTarget.value;
-		} else if (wichInput === 'github') {
-			setData({
-				...data,
-				github: event.currentTarget.value,
-			});
-			data.github = event.currentTarget.value;
-		}
-	};
+    if (wichInput === 'name') {
+      setData({
+        ...data,
+        name: event.currentTarget.value,
+      });
+      data.name = event.currentTarget.value;
+    } else if (wichInput === 'job') {
+      setData({
+        ...data,
+        job: event.currentTarget.value,
+      });
+      data.job = event.currentTarget.value;
+    } else if (wichInput === 'phone') {
+      setData({
+        ...data,
+        phone: event.currentTarget.value,
+      });
+      data.phone = event.currentTarget.value;
+    } else if (wichInput === 'email') {
+      setData({
+        ...data,
+        email: event.currentTarget.value,
+      });
+      data.email = event.currentTarget.value;
+    } else if (wichInput === 'linkedin') {
+      setData({
+        ...data,
+        linkedin: event.currentTarget.value,
+      });
+      data.linkedin = event.currentTarget.value;
+    } else if (wichInput === 'github') {
+      setData({
+        ...data,
+        github: event.currentTarget.value,
+      });
+      data.github = event.currentTarget.value;
+    }
+  };
 
-	const handleCollapsable1 = (event) => {
-		console.log(event.currentTarget.id);
-		if (mode1 === 'hidden') {
-			setMode1('');
-			setRotate1('down');
-		} else {
-			setMode1('hidden');
-			setRotate1('up');
-		}
-	};
+  const handleCollapsable1 = (event) => {
+    console.log(event.currentTarget.id);
+    if (mode1 === 'hidden') {
+      setMode1('');
+      setRotate1('down');
+    } else {
+      setMode1('hidden');
+      setRotate1('up');
+    }
+  };
 
-	const handleCollapsable2 = (event) => {
-		console.log(event.currentTarget.id);
-		if (mode2 === 'hidden') {
-			setMode2('');
-			setRotate2('down');
-		} else {
-			setMode2('hidden');
-			setRotate2('up');
-		}
-	};
+  const handleCollapsable2 = (event) => {
+    console.log(event.currentTarget.id);
+    if (mode2 === 'hidden') {
+      setMode2('');
+      setRotate2('down');
+    } else {
+      setMode2('hidden');
+      setRotate2('up');
+    }
+  };
 
-	const handleCollapsable3 = (event) => {
-		console.log(event.currentTarget.id);
-		if (mode3 === 'hidden') {
-			setMode3('');
-			setRotate3('down');
-		} else {
-			setMode3('hidden');
-			setRotate3('up');
-		}
-	};
+  const handleCollapsable3 = (event) => {
+    console.log(event.currentTarget.id);
+    if (mode3 === 'hidden') {
+      setMode3('');
+      setRotate3('down');
+    } else {
+      setMode3('hidden');
+      setRotate3('up');
+    }
+  };
 
-	return (
+  return (
     <div>
       <Header />
 
-      <main className="page">
+      <main className='page'>
         <Preview
           palettePreview={palette}
           dataPreview={data}
---
+++ src/service/Api.js
@@ -0,0 +1,32 @@
+const postDataFetch = (dataState) => {
+  return fetch('https://awesome-profile-cards.herokuapp.com/card', {
+    method: 'POST',
+    body: JSON.stringify(dataState),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+      console.log(data);
+    });
+};
+
+export default postDataFetch;
+/*twitterShare.classList.remove('hiddenTwitter');
+      lineTwitter.classList.add('hiddenLine');
+      if (data.success === true) {
+        textURL.innerHTML = data.cardURL;
+        textURL.setAttribute('href', data.cardURL);
+        buttonTwitterLink.href = `https://twitter.com/intent/tweet?url=${data.cardURL}`;
+      } else {
+        let textNoURL = `Por favor revise los datos introducidos`;
+        textTitle.innerHTML = `Error al crear la tarjeta:`;
+        const newItem = document.createElement('a');
+        const newContent = document.createTextNode(textNoURL);
+        newItem.appendChild(newContent);
+        textURL.appendChild(newItem);
+        buttonTwitter.remove();
+        textURL.removeAttribute('href');
+      }*/
